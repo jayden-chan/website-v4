52f31aab69b8646afa21e619beca50f148542595
Jayden Chan <jaydencn7@gmail.com>
2019-08-07 16:13:40 -0700

    JC: Better cheatsheet card management

--- src/pages/cheat/Struggle.tsx
+++ src/pages/cheat/Struggle.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import CheatPage from '../../components/CheatPage';
 
 // @ts-ignore
-import {cards, title} from '../../../content/cheatsheets/struggle.toml';
+import {cards as linux} from '../../../content/cheatsheets/linux.toml';
+// @ts-ignore
+import {cards as oneliners} from '../../../content/cheatsheets/oneliners.toml';
+// @ts-ignore
+import {cards as testing} from '../../../content/cheatsheets/test.toml';
+
+function cardFilter(card: any): boolean {
+  return !!card.struggle;
+}
 
 const Struggle: React.FC = () => {
-  return <CheatPage items={cards} title={title} />;
+  const cards = []
+    .concat(linux.filter(cardFilter))
+    .concat(oneliners.filter(cardFilter))
+    .concat(testing.filter(cardFilter));
+  return <CheatPage items={cards} title="SETTMMTOHTS" />;
 };
 
 export default Struggle;
