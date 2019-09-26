b42c4793614c96215425e37102ac42afc747f141
Jayden Chan <jaydencn7@gmail.com>
2019-09-26 12:32:36 -0700

    JC: Fix URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = print_mode => {
   return {
-    URL: JSON.stringify('jayden-chan.github.io/website-tmp'),
+    URL: JSON.stringify('jayden-chan.me'),
     OUTPUT_DIR: JSON.stringify('./build'),
     PRINT_MODE: true,
     TEXT_COLOR: print_mode ? 'black' : 'white',
