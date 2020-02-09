4e5d5f937766398ce5aee42ac93efcc809abbda5
Jayden Chan <jaydencn7@gmail.com>
2020-02-08 20:05:20 -0800

    JC: Revamp cheatsheets layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,77 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import Home from './pages/Home';
-import Resume from './pages/Resume';
-import CoverLetter from './pages/CoverLetter';
-import About from './pages/About';
-import Cheat from './pages/cheat/index';
-import Programming from './pages/cheat/Programming';
-import Struggle from './pages/cheat/Struggle';
-import Linux from './pages/cheat/Linux';
-import OneLiners from './pages/cheat/OneLiners';
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import CoverLetter from "./pages/CoverLetter";
+import About from "./pages/About";
+import Cheat from "./pages/Cheat";
 
 export const SITE_LAYOUT = {
-  relativePath: '/',
-  title: 'Jayden Chan',
-  template: 'landing',
+  relativePath: "/",
+  title: "Jayden Chan",
+  template: "landing",
   component: <Home />,
   subpages: [
     {
-      relativePath: 'resume/',
-      title: 'Resume - Jayden Chan',
-      template: 'resume',
+      relativePath: "resume/",
+      title: "Resume - Jayden Chan",
+      template: "resume",
       component: <Resume />,
-      subpages: [],
+      subpages: []
     },
     {
-      relativePath: 'coverletter/',
-      title: 'Cover Letter',
-      template: 'resume',
+      relativePath: "coverletter/",
+      title: "Cover Letter",
+      template: "resume",
       component: <CoverLetter />,
-      subpages: [],
+      subpages: []
     },
     {
-      relativePath: 'about/',
-      title: 'About - Jayden Chan',
-      template: 'about',
+      relativePath: "about/",
+      title: "About - Jayden Chan",
+      template: "about",
       component: <About />,
-      subpages: [],
+      subpages: []
     },
     {
-      relativePath: 'cheat/',
-      title: 'Cheatsheet',
-      template: 'cheat',
+      relativePath: "cheat/",
+      title: "Cheatsheet",
+      template: "cheat",
       component: <Cheat />,
-      subpages: [
-        {
-          relativePath: 'programming/',
-          title: 'Cheatsheet - Programming',
-          template: 'cheat',
-          component: <Programming />,
-          subpages: [],
-        },
-        {
-          relativePath: 'struggle/',
-          title: 'Cheatsheet - SETTMMTOHTS',
-          template: 'cheat',
-          component: <Struggle />,
-          subpages: [],
-        },
-        {
-          relativePath: 'linux/',
-          title: 'Cheatsheet - Linux',
-          template: 'cheat',
-          component: <Linux />,
-          subpages: [],
-        },
-        {
-          relativePath: 'oneliners/',
-          title: 'Cheatsheet - One-liners',
-          template: 'cheat',
-          component: <OneLiners />,
-          subpages: [],
-        },
-      ],
-    },
-  ],
+      subpages: []
+    }
+  ]
 };
---
+++ src/pages/Cheat.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CheatPage from "../components/CheatPage";
+
+// @ts-ignore
+import { cards as linux } from "../../content/cheatsheets/linux.toml";
+// @ts-ignore
+import { cards as oneliners } from "../../content/cheatsheets/oneliners.toml";
+// @ts-ignore
+import { cards as programming } from "../../content/cheatsheets/programming.toml";
+
+const Cheat: React.FC = () => {
+  const cards = []
+    .concat(linux)
+    .concat(oneliners)
+    .concat(programming);
+  return <CheatPage items={cards} title="Cheatsheet" />;
+};
+
+export default Cheat;
