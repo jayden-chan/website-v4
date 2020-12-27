6623dfbd130d3ee1cb994a5edebc379e6c0110ae
Jayden Chan <jaydencn7@gmail.com>
2020-12-27 01:47:04 -0700

    JC: Search tags with filtering cheatsheet text

--- templates/scripts/filterCheat.js
+++ templates/scripts/filterCheat.js
@@ -1,16 +1,29 @@
 function filterBySearchText() {
   const input = document.getElementById("cheatPageInputID");
-  const text = input.value.toUpperCase();
+  const text = input.value.toLowerCase();
   const ul = document.getElementById("cheatPageListID");
   const li = ul.getElementsByTagName("li");
 
   let a, txtValue;
-  for (let i = 0; i < li.length; i++) {
+  outer: for (let i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("h3")[0];
     txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(text) > -1) {
+    if (txtValue.toLowerCase().indexOf(text) > -1) {
       li[i].style.display = "";
     } else {
+      const tags = li[i].getElementsByTagName("small")[0];
+      const tagsSpans = tags.getElementsByTagName("span");
+      let tag;
+
+      for (let j = 0; j < tagsSpans.length; j++) {
+        tag = tagsSpans[j];
+        txtValue = tag.textContent || tag.innerText;
+        if (txtValue.toLowerCase().indexOf(text) > -1) {
+          li[i].style.display = "";
+          continue outer;
+        }
+      }
+
       li[i].style.display = "none";
     }
   }
