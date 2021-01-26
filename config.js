2398ee5a62ad6cc79664a2113205060f88eabe1f
Jayden Chan <jaydencn7@gmail.com>
2021-01-25 21:18:21 -0700

    JC: Update config

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
 const config = (print_mode) => {
   return {
-    URL: '"jayden-chan.me"',
-    TERM: '"3B"',
-    CSS_BASE: '""',
-    R_CSS_BASE: '".."',
-    OUTPUT_DIR: '"./build"',
     PRINT_MODE: print_mode,
     TEXT_COLOR: print_mode ? "black" : "white",
     BACKGROUND_COLOR: print_mode ? "#e8e6e3" : "#1B1F22",
     // BACKGROUND_COLOR: print_mode ? "#FFFFFF" : "#1B1F22",
-    LINKEDIN: '"jaydencn7"',
-    EMAIL: '"jaydencn7@gmail.com"',
-    GITHUB: '"jayden-chan"',
-    PHONE: '"(403) 874-9705"',
+    ...Object.fromEntries(
+      Object.entries({
+        URL: "jayden-chan.me",
+        TERM: "4A",
+        CSS_BASE: "",
+        R_CSS_BASE: "..",
+        OUTPUT_DIR: "./build",
+        LINKEDIN: "jaydencn7",
+        EMAIL: "jaydencn7@gmail.com",
+        GITHUB: "jayden-chan",
+        PHONE: "(403) 874-9705",
+      }).map(([key, val]) => [key, `"${val}"`])
+    ),
   };
 };
 
